6d5a039af6d26eff7adfb8bd28d73fa9654c4f62
Amir Yunas <ayunas@gmail.com>
2019-12-06 11:25:25 -0600

    finished setting up class hierarchy for oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,3 +1,4 @@
+
 # Write classes for the following class hierarchy:
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
@@ -11,9 +12,51 @@
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
 #
-# e.g.
-#
-# class Whatever:
-#     pass
-#
-# Put a comment noting which class is the base class
+class Vehicle:
+    #base superclass
+    def __repr__(self):
+        return f'<Vehicle>'
+
+class GroundVehicle(Vehicle):
+    #base class for ground vehicles
+    def __repr__(self):
+        return f'<GroundVehicle>'
+
+class FlightVehicle(Vehicle):
+    #baes class for flight vehicles
+    def __repr__(self):
+        return f'<FlightVehicle'
+
+class Car(GroundVehicle):
+    def __repr__(self):
+        return f'<Car>'
+
+class Motorcycle(GroundVehicle):
+    def __repr__(self):
+        return f'<Motorcycle>'
+
+class Airplane(FlightVehicle):
+    def __repr__(self):
+        return f'<Airplane>'
+
+class Starship(FlightVehicle):
+    def __repr__(self):
+        return f'<Starship>'
+
+
+v = Vehicle()
+g = GroundVehicle()
+f = FlightVehicle()
+c = Car()
+p = Airplane()
+s = Starship()
+print(v,g,f,c,p,s)
+
+vehicles = [v,g,f,c,p,s]
+for veh in vehicles:
+    print("Classname of instance: ", type(veh).__name__)
+
+
+
+
+# print(type(v).__name__)  #printing out the name of of the class of the instance
